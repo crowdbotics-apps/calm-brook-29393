396d40d01447dc0e0e9d4187d50b5ce3783aa106
Crowdbotics <team@crowdbotics.com>
2021-08-03 13:30:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "calm_brook_29393Identifier",
+  url: "https://calm_brook_29393Identifier.botics.co",
+  api: "https://calm_brook_29393Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
